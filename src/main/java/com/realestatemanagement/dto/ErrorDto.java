219760a11fd9fa65ed6599dee632ad0be391be53
dragan <draganmonalisamihaela@gmail.com>
2024-12-12 11:33:15 +0200

    Refactor ErrorDto

--- src/main/java/com/realestatemanagement/dto/ErrorDto.java
+++ src/main/java/com/realestatemanagement/dto/ErrorDto.java
@@ -1,11 +1,11 @@
-package com.realestatemanagement.dto.response;
+package com.realestatemanagement.dto;
 
 import com.realestatemanagement.constant.TimestampPattern;
 
 import java.time.LocalDateTime;
 
 public record ErrorDto(String message, String timestamp) {
-    
+
     public ErrorDto(String message) {
         this(message, LocalDateTime.now().format(TimestampPattern.FORMATTER));
     }
