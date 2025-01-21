ab84b914106ffdaed8e1015d89a41a070de9de3e
dragan <draganmonalisamihaela@gmail.com>
2025-01-21 15:33:42 +0200

    update RestExceptionHandler

--- src/main/java/com/realestate/exception/RestExceptionHandler.java
+++ src/main/java/com/realestate/exception/RestExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.realestate.exception;
 
 
+import com.realestate.component.ConstraintValidator;
 import com.realestate.dto.ErrorDto;
+import com.realestate.dto.ValidationDto;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @Slf4j
 @Hidden
@@ -36,6 +42,18 @@ public class RestExceptionHandler {
         log.error("bad request: {}", e.getMessage());
 
         return new ErrorDto(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ValidationDto> handle(MethodArgumentNotValidException e) {
+        return ConstraintValidator.buildErrors(e.getBindingResult());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ValidationDto> handle(ConstraintViolationException e) {
+        return ConstraintValidator.buildErrors(e.getConstraintViolations());
     }
 
 }
