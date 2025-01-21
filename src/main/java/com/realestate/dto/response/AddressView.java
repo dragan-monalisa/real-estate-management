87a399633848aa47c7611fab1a7edefa2b5abeca
dragan <draganmonalisamihaela@gmail.com>
2025-01-21 18:06:26 +0200

    add Address

---
+++ src/main/java/com/realestate/dto/response/AddressView.java
@@ -0,0 +1,15 @@
+package com.realestate.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressView {
+
+    private String county;
+    private String city;
+    private String streetName;
+    private int streetNumber;
+
+}
---
+++ src/main/java/com/realestate/entity/Address.java
@@ -0,0 +1,42 @@
+package com.realestate.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(schema = "hs", name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Size(max = 56)
+    @Column(name = "county", nullable = false, length = 56)
+    private String county;
+
+    @NotBlank
+    @Size(max = 56)
+    @Column(name = "city", nullable = false, length = 56)
+    private String city;
+
+    @NotBlank
+    @Size(max = 56)
+    @Column(name = "street_name", nullable = false, length = 56)
+    private String streetName;
+
+    @Min(1)
+    @Column(name = "street_number", nullable = false)
+    private int streetNumber;
+
+}
