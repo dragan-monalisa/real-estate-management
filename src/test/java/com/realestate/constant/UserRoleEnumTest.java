32a7d9448c05d9c672a4c67e18828ec08e2f0fef
dragan <draganmonalisamihaela@gmail.com>
2025-03-25 21:16:16 +0200

    refactor UserRoleEnumTest

--- src/test/java/com/realestate/constant/UserRoleEnumTest.java
+++ src/test/java/com/realestate/constant/UserRoleEnumTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UserRoleEnumTest {
+class UserRoleEnumTest {
 
     @Test
     void getValidRoleTest() {
