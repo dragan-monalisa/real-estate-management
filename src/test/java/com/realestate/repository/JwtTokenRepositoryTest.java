6086599980d3899a67a7c3bfc199013e3a3ade8d
dragan <draganmonalisamihaela@gmail.com>
2025-03-25 16:01:10 +0200

    add tests for JwtTokenRepository

--- src/test/java/com/realestate/repository/JwtTokenRepositoryTest.java
+++ src/test/java/com/realestate/repository/JwtTokenRepositoryTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class JwtTokenRepositoryTest {
+class JwtTokenRepositoryTest {
 
     @Autowired
     private JwtTokenRepository jwtTokenRepository;
