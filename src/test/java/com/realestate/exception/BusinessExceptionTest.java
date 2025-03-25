60c5db3c1af011c81fc2dcc4ff986a584b64d38a
dragan <draganmonalisamihaela@gmail.com>
2025-03-25 15:58:52 +0200

    add tests for BusinessException

--- src/test/java/com/realestate/exception/BusinessExceptionTest.java
+++ src/test/java/com/realestate/exception/BusinessExceptionTest.java
@@ -2,28 +2,18 @@ package com.realestate.exception;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class BusinessExceptionTest {
-
-    @Test
-    void businessExceptionMessageTest() {
-        String message = "Business exception";
-
-        BusinessException businessException = new BusinessException(message);
-
-        assertThat(businessException.getMessage()).isEqualTo(message);
-    }
+class BusinessExceptionTest {
 
     @Test
     void businessExceptionIsThrownTest() {
-        String message = "An error occurred";
 
         assertThatThrownBy(() -> {
-                    throw new BusinessException(message);
-                }
-        ).isInstanceOf(BusinessException.class).hasMessage(message);
+            throw new BusinessException("message");
+        })
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("message");
     }
 
 }
