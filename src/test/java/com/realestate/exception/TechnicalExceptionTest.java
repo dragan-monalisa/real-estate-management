b33321a530f3ce0224d34079903527e5d121d5fb
dragan <draganmonalisamihaela@gmail.com>
2025-03-25 21:41:18 +0200

    refactor TechnicalExceptionTest

--- src/test/java/com/realestate/exception/TechnicalExceptionTest.java
+++ src/test/java/com/realestate/exception/TechnicalExceptionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class TechnicalExceptionTest {
+class TechnicalExceptionTest {
 
     @Test
     void technicalExceptionIsThrownTest() {
