677316b18e94eb0ca7fc54de7c6e67ff82e844aa
dragan <draganmonalisamihaela@gmail.com>
2025-03-26 18:46:11 +0200

    refactor UserRepositoryTest

--- src/test/java/com/realestate/repository/UserRepositoryTest.java
+++ src/test/java/com/realestate/repository/UserRepositoryTest.java
@@ -3,24 +3,21 @@ package com.realestate.repository;
 import com.realestate.constant.UserRoleEnum;
 import com.realestate.entity.User;
 import jakarta.persistence.EntityManager;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
-@Slf4j
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class UserRepositoryTest {
+class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
@@ -33,6 +30,8 @@ public class UserRepositoryTest {
 
     @BeforeEach
     void setup() {
+        userRepository.deleteAll();
+
         validUser = User.builder()
                 .email("test@email.com")
                 .password("test")
@@ -65,6 +64,7 @@ public class UserRepositoryTest {
 
     @Test
     void existByEmailTest() {
+
         // when
         boolean exists = userRepository.existsByEmail(validUser.getEmail());
 
@@ -74,6 +74,7 @@ public class UserRepositoryTest {
 
     @Test
     void findByEmailAndIsEnabledTrueAndIsLockedFalseTest() {
+
         // when
         Optional<User> optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(validUser.getEmail());
 
@@ -84,58 +85,34 @@ public class UserRepositoryTest {
 
     @Test
     void enableUserTest() {
-        Optional<User> optionalUser = userRepository.findUnconfirmedUser(disabledUser.getEmail());
+
+        // when
+        userRepository.enableUser(disabledUser.getId());
+
+        Optional<User> optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(disabledUser.getEmail());
+
+        // then
         assertThat(optionalUser).isPresent();
-
-        var user = optionalUser.get();
-
-        assertThat(user.getIsEnabled()).isFalse();
-        assertThat(user.getConfirmedAt()).isNull();
-
-        LocalDateTime updatedAt = user.getUpdatedAt();
-
-        userRepository.enableUser(user.getId());
-        entityManager.flush();
-        entityManager.clear();
-
-        optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(user.getEmail());
-
-        assertThat(optionalUser).isPresent();
-
-        user = optionalUser.get();
-
-        assertThat(user.isEnabled()).isTrue();
-        assertThat(user.getConfirmedAt()).isNotNull();
-        assertThat(user.getUpdatedAt()).isAfter(updatedAt);
+        assertThat(optionalUser.get().isEnabled()).isTrue();
     }
 
     @Test
     void changePasswordTest() {
-        Optional<User> optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(validUser.getEmail());
-        assertThat(optionalUser).isPresent();
 
-        var user = optionalUser.get();
-
-        assertThat(user.getPassword()).isEqualTo("test");
-
-        LocalDateTime updatedAt = user.getUpdatedAt();
-        String newPassword = "newPassword";
-
-        userRepository.changePassword(validUser.getEmail(), newPassword);
-        entityManager.flush();
+        // when
+        userRepository.changePassword(validUser.getEmail(), "newPassword");
         entityManager.clear();
 
-        optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(validUser.getEmail());
+        Optional<User> optionalUser = userRepository.findByEmailAndIsEnabledTrueAndIsLockedFalse(validUser.getEmail());
+
+        // then
         assertThat(optionalUser).isPresent();
-
-        user = optionalUser.get();
-
-        assertThat(user.getPassword()).isEqualTo(newPassword);
-        assertThat(user.getUpdatedAt()).isAfter(updatedAt);
+        assertThat(optionalUser.get().getPassword()).isEqualTo("newPassword");
     }
 
     @Test
     void getByEmailTest() {
+
         // when
         Throwable thrown = catchThrowable(() -> userRepository.getByEmail("random"));
 
@@ -146,6 +123,7 @@ public class UserRepositoryTest {
 
     @Test
     void getUnconfirmedUserTest() {
+
         // when
         Throwable thrown = catchThrowable(() -> userRepository.getByEmail(disabledUser.getEmail()));
 
